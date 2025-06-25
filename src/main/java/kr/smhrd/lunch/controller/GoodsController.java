b3ead737bec8f82e98e8e249397d80f0539bc491
JG <wprkf1005@gmail.com>
2025-06-25 11:40:59 +0900

    feat : db정보 수정

--- src/main/java/kr/smhrd/lunch/controller/GoodsController.java
+++ src/main/java/kr/smhrd/lunch/controller/GoodsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://3.35.54.39:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
